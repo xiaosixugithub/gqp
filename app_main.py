1684222204e71d99604e2df583241ee8276728b2
yt7589 <yt7589@qq.com>
2018-11-25 10:20:04 +0800

    initial import

---
+++ app_main.py
@@ -0,0 +1,9 @@
+from spp_engine import SppEngine
+
+def main():
+    sppEngine = SppEngine()
+    sppEngine.startup()
+
+if '__main__' == __name__:
+    main()
+
---
+++ sdps_ds.py
@@ -0,0 +1,12 @@
+# 股票每日开盘价、最高、最低、收盘、成交量的数据集
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class SdpsDs(object):
+    def __init__(self):
+        self.cls_name = 'SdpsDs'
+
+    @staticmethod
+    def draw_kline():
+        print('绘制股票每日价格K线图')
