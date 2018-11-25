f891ee09960b260eeffe3b7d1bd77a19e156b1a9
yt7589 <yt7589@qq.com>
2018-11-25 14:23:11 +0800

    draw kline of stock

--- sdps_ds.py
+++ sdps_ds.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter, WeekdayLocator, DayLocator, MONDAY, date2num
-from matplotlib.finance import candlestick_ohlc
+from mpl_finance import candlestick_ohlc
 from datetime import datetime
 
 class SdpsDs(object):
@@ -13,21 +13,21 @@ class SdpsDs(object):
     @staticmethod
     def draw_kline():
         print('绘制股票每日价格K线图')
-        datas = pd.read_csv('dataset_2.csv')
-        datas = datas.iloc[:, 1:]
-        datas.Date = [date2num( datetime.strptime(date, '%Y-%m-%d') ) for date in datas.Date]
+        raw_datas = pd.read_csv('dataset_2.csv')
+        datas = raw_datas.iloc[-30:, 1:]
+        datas.date = [date2num( datetime.strptime(date, '%Y/%m/%d') ) for date in datas.date]
         recs = list()
         for i in range(len(datas)):
             recs.append(datas.iloc[i, :])
-        ax = plot.subplot()
+        ax = plt.subplot()
         mondays = WeekdayLocator(MONDAY)
         weekFormatter = DateFormatter('%y %b %d')
         ax.xaxis.set_major_locator(mondays)
-        ax.xaxis.set_minor_location(DayLocator())
+        ax.xaxis.set_minor_locator(DayLocator())
         ax.xaxis.set_major_formatter(weekFormatter)
         plt.rcParams['font.sans-serif'] = ['SimHei']
         plt.rcParams['axes.unicode_minus'] = False
-        ax.set_title('上证综指K线图')
+        ax.set_title('上证综指K线图', fontproperties='SimSun')
         candlestick_ohlc(ax, recs, width=0.7, colorup='r', colordown='g')
-        plt.setp(plt.gca().get_xticklabels(), rotation=50, horizatalalignment='center')
+        plt.setp(plt.gca().get_xticklabels(), rotation=50, horizontalalignment='center')
         plt.show()
